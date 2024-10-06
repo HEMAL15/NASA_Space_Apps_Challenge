08132d35aaaad9a72b551b8c49d011201bd107d4
HEMAL15 <141436920+HEMAL15@users.noreply.github.com>
2024-10-06 18:53:30 +0530

    Add files via upload

---
+++ NASA QUIZ/js/questions.js
@@ -0,0 +1,113 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is an Exoplanet?",
+    answer: "A planet outside our solar system",
+    options: [
+      "A planet in our solar syatem",
+      "A planet outside our solar system",
+      "A star that orbits the sun",
+      "One of earths moon"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the name of the method used to discover many exoplanets?",
+    answer: "Transit method",
+    options: [
+      "Transit method",
+      "Gravitational pull method",
+      "Direct imaging",
+      "Exploration"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the habitable zone?",
+    answer: "The region around a star where a planet could have liquid water",
+    options: [
+      "The region around a star where a planet could have liquid water",
+      "The area of space where stars and planets form",
+      "The region around a star where planets are too hot to support life",
+      "A region very far away from the sun"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which telescope has helped discover many exoplanets?",
+    answer: "Kepler Space Telescope",
+    options: [
+      "James Webb Space ",
+      "Galilean Telescope",
+      "Hubble Space Telescope",
+      "Kepler Space Telescope"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is one of the key ingredients for life that scientists look for on exoplanets?",
+    answer: "Liquid water",
+    options: [
+      "Liquid water",
+      "Lava",
+      "Methane",
+      "Oxygen"
+    ]
+  },
+  {
+    numb: 6,
+    question: "How do scientists detect the wobble of a star to find exoplanets?",
+    answer: "By measuring shifts in the star’s light spectrum",
+    options: [
+      "By measuring shifts in the star’s light spectrum ",
+      " By observing the star’s color changes",
+      "By measuring the star’s temperature",
+      "By finding out the star's age"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What type of planet is a gas giant like Jupiter?",
+    answer: "A large planet with no solid surface",
+    options: [
+      " A small, rocky planet",
+      "A large planet with no solid surface",
+      " A planet with water oceans and landmasses",
+      "A planet closest to the sun"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does it mean if a planet is in the ‘habitable zone’?",
+    answer: "It is in a region where liquid water might exist",
+    options: [
+      "It has an atmosphere like Earth’s",
+      " It is close enough to its star to melt ice",
+      "It is in a region where liquid water might exist",
+      "It has its own moon"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What do scientists use space telescopes for when studying exoplanets?",
+    answer: "To measure a planet’s size, temperature, and atmosphere",
+    options: [
+      " To see the exoplanets directly with visible light",
+      "To measure a planet’s size, temperature, and atmosphere",
+      "To find comets near the planet",
+      "To look at satellites"
+    ]
+  },
+  {
+    numb: 10,
+    question: "What is one of the key goals of exoplanet exploration?",
+    answer: "To discover planets that could support life?",
+    options: [
+      " To find stars that are close to Earth",
+      " To discover planets that could support life",
+      "To study black holes near planets",
+      "To send man to space"
+    ]
+  },
+  
+];
